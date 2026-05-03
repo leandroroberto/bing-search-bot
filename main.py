ce23a3071a353f91006f247707689a11b8c24348
Leandro <lecodriver@hotmail.com>
2026-05-03 00:42:00 -0300

    Melhoria na navegação e remoção de duplicados

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-import time
 
 #Inicializa o webdriver com chrome
 navegador = webdriver.Chrome()
@@ -15,7 +14,9 @@ navegador.get("https://www.bing.com/")
 navegador.maximize_window()
 
 #Clica no botão de aceitar os termos
-WebDriverWait(navegador, 5).until(EC.presence_of_element_located((By.ID, "bnp_btn_accept"))).click()
+botao_termos = WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.ID, "bnp_btn_accept")))
+
+navegador.execute_script("arguments[0].click();", botao_termos)
 
 #Armazena o elemento "barra de pesquisa na variável" e aguarda esse elemento aparecer na tela
 barra_pesquisa = WebDriverWait(navegador, 5).until(EC.presence_of_element_located((By.ID, "sb_form_q")))
@@ -42,19 +43,27 @@ for resultado in resultados:
 #Rola a página até o final
 navegador.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-#Clica na segunda página da pesquisa
-navegador.find_element(By.CLASS_NAME, "sb_bp").click()
+url_antiga = navegador.current_url
+
+#Localiza o botão da página 2
+botao_pagina_2 = navegador.find_element(By.CSS_SELECTOR, ".b_widePag.sb_bp")
+
+#Clica no botão
+navegador.execute_script("arguments[0].click();", botao_pagina_2)
 
 #Aguarda os elementos da próxima página
-WebDriverWait(navegador, 5).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.b_algo h2")))
+WebDriverWait(navegador, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.b_algo h2")))
 
-novos_resultados= navegador.find_elements(By.CSS_SELECTOR, "li.b_algo h2")
+novos_resultados = navegador.find_elements(By.CSS_SELECTOR, "li.b_algo h2")
 
 for novo_titulo in novos_resultados:
     titulos.append(novo_titulo.text)
 
+titulos = list(set(titulos))
 
 for titulo in titulos:
     print(titulo)
 
-time.sleep(30)
+input("Pressione ENTER para fechar...")
+
+navegador.quit()
